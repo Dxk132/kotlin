b8ec73b71c545de4fe43bed748651f750ca3b9db
DxK132 <sebas.casalfa@gmail.com>
2025-03-24 11:06:52 -0600

    se hizo el proyecto2

---
+++ src/1_tipos_de_variables/Ejemplos.kt
@@ -0,0 +1,31 @@
+package `1_tipos_de_variables`
+
+fun main() {
+    //val=inmutable var=mutable
+    //variable inmutamble de tipo entrero
+    val edad: Int
+    edad = 48
+    //variable inmutable de tipo decimal
+    val sueldos: Float
+    sueldos = 1200.55f
+    //variable inmutable de tipo decimal
+    val total: Double
+    total = 70000.24
+
+    //variable inmutable de tipo alfanumerico
+    val titulo: String
+    titulo = "sistema de ventanas"
+
+    //variable mutable de tipo entero
+    var mes: Int
+    mes = 1
+    //algunas lineas de codigo
+    mes = 2
+
+    //char almacena un solo caracter (usar con ')
+    var continua: Char = 's'
+
+    //variable de tipo booleano, true o false
+    var fin: Boolean = false
+
+}
---
+++ src/1_tipos_de_variables/proyecto2.kt
@@ -0,0 +1,29 @@
+package `1_tipos_de_variables`
+
+fun main(){
+    val num1: Int
+    num1=5
+    val num2: Int
+    num2=5
+    var suma: Int
+    suma= num1+num2
+    print("la suma de ")
+    print(num1)
+    print(" y ")
+    print(num2)
+    print(" es ")
+    println(suma)
+
+    val num3: Int
+    num3=10
+    val num4: Int
+    num4=10
+    var multip: Int
+    multip= num3*num4
+    print("la multiplicacion de ")
+    print(num3)
+    print(" y ")
+    print(num4)
+    print(" es ")
+    println(multip)
+}
